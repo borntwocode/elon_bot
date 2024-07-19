e85f69ad7d2137fbba79d9012adec14c6efc6d58
Asadbek <braverlrgit@gmail.com>
2024-07-19 09:40:57 +0500

    pachti tugadi

---
+++ src/main/java/uz/pdp/elonbot/controller/BotController.java
@@ -0,0 +1,28 @@
+package uz.pdp.elonbot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.elonbot.bot.BotService;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+public class BotController {
+
+    private final BotService botService;
+
+    @PostMapping("/accepted/{postId}")
+    void changePostAndSendToChannel(@PathVariable UUID postId){
+        botService.sendToChannel(postId, true);
+    }
+
+    @PostMapping("/rejected/{postId}")
+    void deletePostAndNotifyUser(@PathVariable UUID postId){
+        botService.notifyUser(postId);
+    }
+
+}
